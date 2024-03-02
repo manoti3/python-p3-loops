d705f6f71a2da38d072080f137c1bdc0219e3a4d
manoti3 <hillarymanoti0@gmail.com>
2024-03-02 15:00:28 +0300

    completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
+import time
+
 def happy_new_year():
+    for i in range(10, 0, -1):
+        print(i)
+        time.sleep(1)  # Sleep for 1 second between countdown
+    print("Happy New Year!")
+
     # code goes here!
     pass
 
-def square_integers(int_list):
+def square_integers(numbers):
+    return [num ** 2 for num in numbers]
+
     # code goes here!
     pass
 
 def fizzbuzz():
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
     # code goes here!
     pass
